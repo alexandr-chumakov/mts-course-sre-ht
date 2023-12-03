8816e20a5f21d540c88104e616b05ef5b8e402e8
Alexandr Chumakov <aleksandr.tschumakov@gmail.com>
2023-12-03 17:16:43 +0600

    PoC for city base filling

--- scripts/fill_cities.js
+++ scripts/fill_cities.js
@@ -1,10 +1,31 @@
 import http from 'k6/http';
+import { sharedArray } from 'k6/data';
 import { parse } from "k6/x/yaml";
 
 
 const configfile = open("../config.yaml");
 
+// Load file content into a shared array
+const fileContent = sharedArray('fileContent', function () {
+  return open('../data/geo_names_list.txt').read(1 << 30);
+});
+
+
 export default function () {
 	const config = YAML.parse(configfile);
+	const hostname = config.hostname
+	console.log(`Line from file: ${hostname}`);
+	
 	// TODO: POST requests to add all cities from file
+	
+	// Split the file content into lines
+	const lines = fileContent.split('\n');
+
+	// Iterate over lines
+	for (let i = 0; i < 10 /* lines.length */; i++) {
+	  const line = lines[i];
+	  // Process each line here
+	  console.log(`Line from file: ${line}`);
+	}
+	
 }
