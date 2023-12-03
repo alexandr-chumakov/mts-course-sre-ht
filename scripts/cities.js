b32b59b00e72449e5c25f32e9a41ec22f68456a8
Alexandr Chumakov <aleksandr.tschumakov@gmail.com>
2023-12-03 21:06:25 +0600

    added script for the Cities HT profile

---
+++ scripts/cities.js
@@ -0,0 +1,43 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+import { parse } from "k6/x/yaml";
+
+
+const configfile = open("../config.yaml");
+const config = parse(configfile);
+const hostname = config.hostname
+const addressOfAPI = config.address
+	
+export let options = {
+  stages: [
+    { duration: '1m', target: 50 },  // Start with 50 RPS for 1 minute
+    { duration: '1m', target: 100 }, // Increase to 100 RPS for the next 1 minute
+    { duration: '1m', target: 150 }, // Increase to 150 RPS for the next 1 minute
+    { duration: '1m', target: 250 }, 
+    { duration: '1m', target: 500 }, 
+    { duration: '1m', target: 700 }, 
+    { duration: '1m', target: 900 }, 
+    { duration: '1m', target: 1000 }, 
+    { duration: '1m', target: 0 },   // Ramp down to 0 RPS in the last 1 minute
+  ],
+};
+
+export default function () {
+
+	const url = `${addressOfAPI}/Cities`; // Replace with your API endpoint
+	const headers = {
+				'Content-Type': 'application/json',
+				'Host': hostname
+				};
+
+
+	const getResponse = http.get(url, headers);
+
+	// Check if the GET request was successful
+	check(getResponse, {
+	'GET Success': (res) => res.status === 200,
+	});
+
+	// Add some sleep time between iterations
+	sleep(5);
+}
